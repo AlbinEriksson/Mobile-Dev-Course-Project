c762df772acd3556e409a07015ee11988d9d62d8
rikhall1515 <30295873+rikhall1515@users.noreply.github.com>
2020-12-10 20:51:15 +0100

    Update intro.dart

--- lib/screens/intro/intro.dart
+++ lib/screens/intro/intro.dart
@@ -11,17 +11,91 @@ class _IntroState extends State<Intro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Welcome"),
-      ),
+      appBar: PreferredSize(
+          child: SafeArea(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                          Icons.dashboard_rounded,
+                          color: Colors.black,
+                      ),
+                      iconSize: 40.0,
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.settings,
+                        color: Colors.black,
+                      ),
+                      iconSize: 40.0,
+                    )
+                  ],
+                ),
+              ),
+            ),
+      ), preferredSize: Size.fromHeight(100)),
       body: Center(
+        //color: Colors.blue,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            NavButton("Register", Routes.register),
-            NavButton("Login", Routes.login),
-          ],
-        ),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              //color: Colors.blue,
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(20.0),
+              child: Text(
+                'Welcome!',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 40.0,
+                ),
+              ),
+            ),
+            Column (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 300,
+                  child: RaisedButton(
+                    color: Colors.purple,
+                    splashColor: Colors.pink,
+                    child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        )
+                    ),
+                    onPressed: () => Navigator.pushNamed(context, Routes.login, arguments: null),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  width: 300,
+                  child: RaisedButton(
+                    color: Colors.purple,
+                    splashColor: Colors.pink,
+                    child: Text(
+                        'Register',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        )
+                    ),
+                    onPressed: () => Navigator.pushNamed(context, Routes.register, arguments: null),
+                  ),
+                ),
+              ],
+            ),
+          ]
+        )
       ),
     );
   }
