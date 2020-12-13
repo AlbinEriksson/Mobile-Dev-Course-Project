47cd2e0414ca31f9823c111dd56bbfeee0e6b4f5
benti99 <benti.z@hotmail.com>
2020-12-13 17:03:26 +0100

    First Commit.

--- lib/screens/register/register.dart
+++ lib/screens/register/register.dart
@@ -1,22 +1,275 @@
 import 'package:dva232_project/routes.dart';
-import 'package:dva232_project/widgets/back_nav_button.dart';
-import 'package:dva232_project/widgets/replace_nav_button.dart';
+
 import 'package:flutter/material.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  String name; //Dessa bör kunna användas till backend-delen
+  String email;
+  String password;
+  String confirmPassword;
+
+  String role;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Register"),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BackNavButton("Back to front page", Routes.intro),
-            ReplaceNavButton("Register", Routes.home),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(0.0),
+                child: Text(
+                  'Account Info',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple,
+                    fontSize: 40.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 0, horizontal: 16.0),
+                child: Divider(),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                child: TextField(
+                  style: TextStyle(
+                    fontSize: 17.0,
+                  ),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Colors.purple,
+                    ),
+                    labelText: 'Name (Last name is optional)',
+                    labelStyle: TextStyle(
+                      color: Colors.purple,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      name = value;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                child: TextField(
+                  style: TextStyle(
+                    fontSize: 17.0,
+                  ),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.purple,
+                    ),
+                    labelText: 'Email',
+                    labelStyle: TextStyle(
+                      color: Colors.purple,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      name = value;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                child: TextField(
+                  obscureText: true,
+                  style: TextStyle(
+                    fontSize: 17.0,
+                  ),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.purple,
+                    ),
+                    labelText: 'Password',
+                    labelStyle: TextStyle(
+                      color: Colors.purple,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                child: TextField(
+                  obscureText: true,
+                  style: TextStyle(
+                    fontSize: 17.0,
+                  ),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.purple,
+                    ),
+                    labelText: 'Confirm Password',
+                    labelStyle: TextStyle(
+                      color: Colors.purple,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(100.0),
+                      borderSide: BorderSide(
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      confirmPassword = value;
+                    });
+                  },
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                width: 335,
+                height: 60,
+                decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                        color: Colors.purple,
+                        width: 1.0,
+                        style: BorderStyle.solid),
+                    borderRadius: BorderRadius.circular(100.0),
+                  ),
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    isExpanded: true,
+                    hint: Text(
+                      "Select Role",
+                      style: TextStyle(fontSize: 17.0
+                          //fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    value: role,
+                    items: <String>['Student', 'Teacher']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                          style: TextStyle(fontSize: 17.0
+                              //fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (String value) {
+                      setState(() {
+                        role = value;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                //margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                width: 300,
+                height: 100,
+                child: RaisedButton(
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 21,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100.0),
+                  ),
+                  color: Colors.purple,
+                  onPressed: () => Navigator.popAndPushNamed(
+                      context, Routes.home,
+                      arguments: null),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
