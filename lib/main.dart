2eba460fdbed4b22b9c9b5776f421fa9daa20be1
Albin Eriksson <albin-eriksson@outlook.com>
2020-12-17 13:52:52 +0100

    User API methods, login and register

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+import 'dart:io';
+
 import 'package:dva232_project/app.dart';
+import 'package:dva232_project/client/languide_http_overrides.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
+void main() {
+  // Disable bad certificates if the user API has a valid certificate
+  // (Currently, the user API is running on a local machine, without a valid
+  // SSL certificate).
+  HttpOverrides.global = LanGuideHttpOverrides(allowBadCertificates: true);
+  runApp(App());
+}
