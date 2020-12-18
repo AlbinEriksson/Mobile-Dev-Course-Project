0cb3ad46262e44b379da45920f2e8c2f21937e05
rikhall1515 <30295873+rikhall1515@users.noreply.github.com>
2020-12-18 15:49:17 +0100

    Added the backend of the registration process. Now we use regex to validate the usernames, emails, passwords and roles of each user to make sure that no illegal values are passed on to the database. This also comes with good efficiency given the fact that we do not cause a postback everytime we press the registration button. This is because we first verify all the data in the callback function of the button before then passing it into a futurebuilder where it calls upon the UserAPI.

--- lib/screens/register/register.dart
+++ lib/screens/register/register.dart
@@ -1,6 +1,6 @@
 import 'package:dva232_project/routes.dart';
 import 'package:dva232_project/widgets/languide_navbar.dart';
-
+import 'package:dva232_project/client/user_api_client.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -9,12 +9,12 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  String name; //Dessa bör kunna användas till backend-delen
-  String email;
-  String password;
-  String confirmPassword;
-
   String role;
+  var response;
+  final userNameController = new TextEditingController();
+  final emailController = new TextEditingController();
+  final passwordController = new TextEditingController();
+  final confirmPasswordController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -39,14 +39,19 @@ class _RegisterState extends State<Register> {
                     fontSize: 40.0,
                   ),
                 ),
-              ),
+              ), //Title of screen
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 0, horizontal: 16.0),
-                child: Divider(),
-              ),
+                child: Divider(
+                  thickness: 4.0,
+                  indent: 20.0,
+                  endIndent: 20.0,
+                ),
+              ), //Delimiter
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
                 child: TextField(
+                  controller: userNameController,
                   style: TextStyle(
                     fontSize: 17.0,
                   ),
@@ -65,12 +70,6 @@ class _RegisterState extends State<Register> {
                         color: Colors.purple,
                       ),
                     ),
-                    /*focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(100.0),
-                      borderSide: BorderSide(
-                        color: Colors.purple,
-                      ),
-                    ),*/
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(100.0),
                       borderSide: BorderSide(
@@ -78,16 +77,12 @@ class _RegisterState extends State<Register> {
                       ),
                     ),
                   ),
-                  onChanged: (value) {
-                    setState(() {
-                      name = value;
-                    });
-                  },
                 ),
-              ),
+              ), //Username
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
                 child: TextField(
+                  controller: emailController,
                   style: TextStyle(
                     fontSize: 17.0,
                   ),
@@ -110,16 +105,12 @@ class _RegisterState extends State<Register> {
                       borderRadius: BorderRadius.circular(100.0),
                     ),
                   ),
-                  onChanged: (value) {
-                    setState(() {
-                      email = value;
-                    });
-                  },
                 ),
-              ),
+              ), //Email
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
                 child: TextField(
+                  controller: passwordController,
                   obscureText: true,
                   style: TextStyle(
                     fontSize: 17.0,
@@ -146,16 +137,12 @@ class _RegisterState extends State<Register> {
                       ),
                     ),
                   ),
-                  onChanged: (value) {
-                    setState(() {
-                      password = value;
-                    });
-                  },
                 ),
-              ),
+              ), //Password
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
                 child: TextField(
+                  controller: confirmPasswordController,
                   obscureText: true,
                   style: TextStyle(
                     fontSize: 17.0,
@@ -182,13 +169,8 @@ class _RegisterState extends State<Register> {
                       ),
                     ),
                   ),
-                  onChanged: (value) {
-                    setState(() {
-                      confirmPassword = value;
-                    });
-                  },
                 ),
-              ),
+              ), //Password confirmation
               Container(
                 padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
@@ -235,7 +217,7 @@ class _RegisterState extends State<Register> {
                     ),
                   ),
                 ),
-              ),
+              ), //Role selection
               Container(
                 padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
                 width: 300,
@@ -252,11 +234,145 @@ class _RegisterState extends State<Register> {
                     borderRadius: BorderRadius.circular(100.0),
                   ),
                   color: Colors.purple,
-                  onPressed: () => Navigator.popAndPushNamed(
-                      context, Routes.home,
-                      arguments: null),
+                  onPressed: () {
+                    String dialogue = "";
+                    bool register = false;
+                    RegExp userExp = new RegExp(r"^(?=.{8,20}$)(?![_. ])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$");
+                    /*
+                    * Username has to be 8-20 characters long.
+                    * _ or . are not allowed in the beginning or the end, furthermore they're not allowed inside of the username in the form of: __    _.    ._    ..
+                    * a-z, A-Z and 0-9 are all allowed characters.
+                    * With this in mind, a valid username looks like: bigPop99, SkadoodleBadoodle56, diamond_pickaxe1337.
+                    * An invalid username would look like: big..Pop99, kab__00m, ooga._booga.
+                    * //Rikard
+                    * */
+                    RegExp emailExp = new RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+.[a-zA-Z]+");
+                    /*
+                    *Checks if the structure of the email is correct (i.e test@example.com).
+                    *Does not allow for double @ or double .
+                    *Makes sure no illegal double-quotes is used. However, the single-quote is allowed and therefore all strings need to be double-quoted to ensure consistency.
+                    *The domain can only have alphabetical and numerical characters, while the domain type can only have alphabetical characters.
+                    *
+                    * Drawbacks:
+                    *   - The problem is that no established domain lengths are done, so illegal domain lengths are let through.
+                    *   - The above point is also true for the domain type as it is not checked for length.
+                    *
+                    * We realize that these drawbacks exist but choose to not over-complicate the validation. We could simply send an email-verification to that given adress
+                    * and if they don't respond, more than likely their email is either incorrect or the user who created the account is not the real owner.
+                    * //Rikard
+                    * */
+                    RegExp strongPasswordRegex = new RegExp(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+                    RegExp mediumPasswordRegex = new RegExp(r"^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+                    /*
+                    * The strong version of password regex has strict requirements:
+                    *   - At least 1 lowercase alphabetical character
+                    *   - At least 1 uppercase alphabetical character
+                    *   - At least 1 numerical character
+                    *   - At least 1 special character
+                    *   - Has to be at least 8 characters long
+                    *
+                    * The medium version of password regex has less strict guidelines, therefore the regex becomes more complex:
+                    *   - The structure of the regex includes OR, between 3 different combinations of lowercase alphabetical, uppercase alphabetical and numerical characters.
+                    *   - You either have:
+                    *       - 1 lowercase + 1 upper case alphabetical OR
+                    *       - 1 lowercase alphabetical + 1 numerical character OR
+                    *       - 1 uppercase alphabetical + 1 numerical character
+                    *   - Has to be at least 6 characters long
+                    *
+                    * For simplicity we leave out special characters out of the medium version. The reason we don't include a weak version of the regex is also for
+                    * simplicity's sake. Because the more lenient we are with the password strength, the complexity of the regex proportionally increases. Furthermore
+                    * it's worth noting that we'd like to promote good password security to our potential customers, even if it means that it becomes less convenient
+                    * for them to create an account to meet our security standards.
+                    * //Rikard
+                    * */
+
+                    if(userNameController.text.isEmpty || emailController.text.isEmpty || passwordController.text.isEmpty || confirmPasswordController.text.isEmpty || role.length == 0){
+                      dialogue = "One or more fields are empty.";
+                    }
+                    else if(!userExp.hasMatch(userNameController.text)){
+                      dialogue = "Invalid username.";
+                    }
+                    else if(!emailExp.hasMatch(emailController.text)){
+                      dialogue = "Invalid email.";
+                    }
+                    else if(!strongPasswordRegex.hasMatch(passwordController.text) && !mediumPasswordRegex.hasMatch(passwordController.text)){
+                      dialogue = "Password is too weak!";
+                    }
+                    else if(!(passwordController.text == confirmPasswordController.text)){
+                      dialogue = "Your password confirmation was not the same as the password.";
+                    }
+                    else{
+                      register = true;
+                    }
+                    if(!register){
+                      return showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text(dialogue),
+                          );
+                        },
+                      );
+                    } //If any of the above conditions were unmet, the user does not get to create an account
+                    else{
+                      response = UserAPIClient.register(emailController.text, passwordController.text, userNameController.text, "", role);
+                    }//If however the conditions were met, then they are free to access the rest of the app
+                  },
                 ),
-              ),
+              ), //Register button
+              FutureBuilder(
+                future: response,
+                builder: (context, data){
+                  if(data.hasData) {
+                    if (data.data["message"] == "user was registered") {
+                      Navigator.popAndPushNamed(
+                          context, Routes.home,
+                          arguments: null);
+                    }
+                    else if (data.data["message"] == "you must specify 'email', 'password', 'firstName' and 'role' in request body") {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text("Error with specification."),
+                          );
+                        },
+                      );
+                    }
+                    else if (data.data["message"] == "invalid role") {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text("Invalid role."),
+                          );
+                        },
+                      );
+                    }
+                    else if (data.data["message"] == "that email is already in use") {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text("That email is already in use."),
+                          );
+                        },
+                      );
+                    }
+                    else if (data.data["message"] == "failed to register user") {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text("Failed to register the user."),
+                          );
+                        },
+                      );
+                    }
+                  }
+                  return SizedBox.shrink();
+                }
+              )
             ],
           ),
         ),
