b47d5e1499cd829176fd2a3d22bf934a7f0a0bfe
Albin Eriksson <albin-eriksson@outlook.com>
2020-12-25 10:33:59 +0100

    Moved styling from intro.dart

--- lib/screens/intro/intro.dart
+++ lib/screens/intro/intro.dart
@@ -1,7 +1,6 @@
 import 'package:dva232_project/client/user_api_client.dart';
 import 'package:dva232_project/routes.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 class Intro extends StatefulWidget {
   @override
@@ -9,93 +8,57 @@ class Intro extends StatefulWidget {
 }
 
 class _IntroState extends State<Intro> {
-
   Widget _content(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-            SizedBox(height: 20),
-            Container(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(height: 20),
+              Container(
                 padding: EdgeInsets.all(30.0),
                 margin: EdgeInsets.all(30.0),
-                child: Image(image: AssetImage('images/logo.png'))),
-            Container(
-              //color: Colors.blue,
-              child: Text(
-                'Welcome!',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  fontSize: 40.0,
+                child: Image(
+                  image: AssetImage('images/logo.png'),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: 300,
-              height: 50,
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(100),
+              Center(
+                //color: Colors.blue,
+                child: Text(
+                  'Welcome!',
+                  style: Theme.of(context).textTheme.headline3,
                 ),
-                color: Colors.purple,
-                splashColor: Colors.pink,
-                child: Text('Login',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: Colors.white,
-                      fontSize: 21.0,
-                    )),
-                onPressed: () =>
-                    Navigator.pushNamed(context, Routes.login, arguments: null),
               ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: 300,
-              height: 50,
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(100),
-                ),
-                color: Colors.purple,
-                splashColor: Colors.pink,
-                child: Text('Register',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: Colors.white,
-                      fontSize: 20.0,
-                    )),
+              SizedBox(height: 10),
+              ElevatedButton(
+                child: Text('Login'),
+                onPressed: () => Navigator.pushNamed(context, Routes.login),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                child: Text('Register'),
                 onPressed: () => Navigator.pushNamed(context, Routes.register,
                     arguments: null),
               ),
-            ),
-          ])),
+            ]),
+      ),
     );
   }
-  /// - clientError
-  /// - noRefreshToken
-  /// - refreshTokenExpired
-  /// - success
-  /// - unknown
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return FutureBuilder(
-      future: UserAPIClient.refresh(),
-      builder: (context, data){
-        if(data.hasData){
-          if(data.data == UserAPIResult.success){
-            Navigator.pushNamed(context, Routes.login, arguments: null);
+        future: UserAPIClient.refresh(),
+        builder: (context, data) {
+          if (data.hasData) {
+            if (data.data == UserAPIResult.success) {
+              Navigator.pushNamed(context, Routes.login, arguments: null);
+            }
+            return _content(context);
+          } else {
+            return Center(child: CircularProgressIndicator());
           }
-          return _content(context);
-        }
-        else{
-          return Center(child: CircularProgressIndicator());
-        }
-      }
-    );
+        });
   }
 }
-
--- lib/theme.dart
+++ lib/theme.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class LanGuideTheme {
   static ThemeData data() => ThemeData(
     primarySwatch: Colors.purple,
-
+    // accentColor: Colors.purpleAccent,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100.0),
+        ),
+        minimumSize: Size.fromHeight(50),
+        elevation: 5.0,
+      ),
+    ),
+    textTheme: TextTheme(
+      headline3: TextStyle(
+        color: Colors.black,
+      ),
+      button: TextStyle(
+        color: Colors.white,
+        fontSize: 21,
+        fontWeight: FontWeight.normal,
+      ),
+    ),
   );
 }
