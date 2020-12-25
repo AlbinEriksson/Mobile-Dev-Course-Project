c64ba74121c5e903cf97243c0c419ccfe05d2358
adambostrom <adambostrom93@gmail.com>
2020-12-25 11:34:27 +0100

    listening web sound

--- lib/screens/tests/listening/listening_audio.dart
+++ lib/screens/tests/listening/listening_audio.dart
@@ -1,3 +1,4 @@
+import 'package:http/http.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'package:flutter/services.dart' show ByteData, rootBundle;
@@ -6,22 +7,34 @@ import 'package:audioplayers/audio_cache.dart';
 
 
 class ListeningAudio extends AudioPlayer{
-  Duration _duration=Duration();
-  Duration _position=Duration();
+
+  String _testUrl="https://luan.xyz/files/audio/ambient_c_motion.mp3";
+  String localFilePath;
 
   String mp3Uri='';
-  AudioPlayer player = AudioPlayer();
-  AudioCache audioCache = AudioCache(prefix: 'assets/sneeze.mp3');
-
+  AudioPlayer player =AudioPlayer();
+  //
   ListeningAudio({this.player});
 
-  void loadSound() async{
-    final ByteData data = await rootBundle.load('assets/sneeze.mp3');
-    Directory tempDir =await getTemporaryDirectory();
-    File tempFile=File('${tempDir.path}/sneeze.mp3}');
-    await tempFile.writeAsBytes(data.buffer.asUint8List(), flush:true);
-    mp3Uri=tempFile.uri.toString();
+  Future initPlayer() async{
+    // final ByteData data = await rootBundle.load('assets/sneeze.mp3');
+    // Directory tempDir =await getTemporaryDirectory();
+    // File tempFile=File('${tempDir.path}/sneeze.mp3}');
+    // await tempFile.writeAsBytes(data.buffer.asUint8List(), flush:true);
+    //mp3Uri=tempFile.uri.toString();
+    final bytes = await readBytes(_testUrl);
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/audio.mp3');
+    await file.writeAsBytes(bytes.buffer.asUint8List(), flush:true);
+    // await file.writeAsBytes(bytes);
+    // if (await file.exists()) {
+    //   setState(() {
+    //     localFilePath = file.path;
+    //   });
+    // }
+    mp3Uri=file.uri.toString();
   }
+
   int playSound(int _click) {
     if(_click==0) {
       player.play(mp3Uri);
@@ -32,10 +45,11 @@ class ListeningAudio extends AudioPlayer{
     }
     return _click;
   }
-
-
-
+  //
 
 
 
 }
+
+
+
