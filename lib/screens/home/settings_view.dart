b2c14353c17b37cad53eecaceadb03219d6052ed
benti99 <benti.z@hotmail.com>
2020-12-26 13:54:29 +0100

    Added a view for settings, and updated dependencies to be able to use font_awesome icons.

--- lib/screens/home/settings_view.dart
+++ lib/screens/home/settings_view.dart
@@ -1,7 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SettingsView extends StatelessWidget {
+class SettingsView extends StatefulWidget {
   @override
-  Widget build(BuildContext context) =>
-      Center(child: Text("Not implemented"));
+  _SettingsViewState createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.person,
+                    color: Colors.purple,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Account",
+                    style: TextStyle(
+                      fontSize: 23,
+                      //fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                  )
+                ],
+              ),
+            ),
+            Divider(
+              height: 50,
+              thickness: 2,
+              indent: 0,
+              endIndent: 0,
+            ),
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.lock,
+                    color: Colors.purple,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Privacy & Security",
+                    style: TextStyle(
+                      fontSize: 23,
+                      //fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                  )
+                ],
+              ),
+            ),
+            Divider(
+              height: 50,
+              thickness: 2,
+              indent: 0,
+              endIndent: 0,
+            ),
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Row(
+                children: [
+                  Icon(
+                    FontAwesomeIcons.globe,
+                    color: Colors.purple,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Language",
+                    style: TextStyle(
+                      fontSize: 23,
+                      //fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                  )
+                ],
+              ),
+            ),
+            Divider(
+              height: 50,
+              thickness: 2,
+              indent: 0,
+              endIndent: 0,
+            ),
+            GestureDetector(
+              onTap: (){
+
+              },
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.nightlight_round,
+                    color: Colors.purple,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Light/Dark theme",
+                    style: TextStyle(
+                      fontSize: 23,
+                      //fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacer(),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                  )
+                ],
+              ),
+            ),
+            Divider(
+              height: 50,
+              thickness: 2,
+              indent: 0,
+              endIndent: 0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
