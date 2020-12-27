04bb22f5c6e04953bf0b326079a6f457090f47ec
Albin Eriksson <albin-eriksson@outlook.com>
2020-12-27 15:27:42 +0100

    Use stack and container alignment in navbar

--- lib/widgets/languide_navbar.dart
+++ lib/widgets/languide_navbar.dart
@@ -1,3 +1,4 @@
+import 'package:dva232_project/routes.dart';
 import 'package:flutter/material.dart';
 
 class LanGuideNavBar extends StatelessWidget with PreferredSizeWidget {
@@ -13,31 +14,29 @@ class LanGuideNavBar extends StatelessWidget with PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    var topWidgets = <Widget>[];
-
-    if (showBackIcon) {
-      topWidgets.add(IconButton(
-        icon: Icon(Icons.arrow_back),
-        iconSize: 40.0,
-        onPressed: () => _backIconPressed(context),
-      ));
-    }
-
-    if (showFlag) {
-      topWidgets.add(IconButton(
-        icon: Image.asset("icons/flags/png/gb.png", package: "country_icons"),
-        iconSize: 60.0,
-      ));
-    }
-
     return AppBar(
       automaticallyImplyLeading: false,
       actions: [
         Expanded(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: topWidgets,
+          child: Stack(
+            children: [
+              if(showBackIcon) Container(
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  iconSize: 40.0,
+                  onPressed: () => _backIconPressed(context),
+                ),
+              ),
+              if(showFlag) Container(
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                  icon: Image.asset("icons/flags/png/gb.png", package: "country_icons"),
+                  iconSize: 60.0,
+                  onPressed: () => Navigator.pushNamed(context, Routes.languageSelection),
+                ),
+              )
+            ],
           ),
         ),
       ],
