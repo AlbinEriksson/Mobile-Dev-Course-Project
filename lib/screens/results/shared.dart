c4053ed40af451518dc21344e6573f221deacf11
Albin Eriksson <albin-eriksson@outlook.com>
2020-12-30 17:42:03 +0100

    Extract shared code in results pages

---
+++ lib/screens/results/shared.dart
@@ -0,0 +1,76 @@
+import 'package:dva232_project/client/user_api_client.dart';
+import 'package:dva232_project/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+Widget resultsDisplay(
+    BuildContext context, double accuracy, int score, int maxScore) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      Text(
+        AppLocalizations.of(context).yourScoreIs,
+        style: Theme.of(context).textTheme.headline4,
+      ),
+      Text(
+        "${(100 * accuracy).toStringAsFixed(0)}%",
+        style: Theme.of(context).textTheme.headline1,
+      ),
+      Text(AppLocalizations.of(context)
+          .correctAnswers
+          .replaceAll("\$1", "$score")
+          .replaceAll("\$2", "$maxScore")),
+    ],
+  );
+}
+
+Widget backHomeButton(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0),
+    child: ElevatedButton(
+      onPressed: () {
+        Navigator.popUntil(context, ModalRoute.withName(Routes.home));
+      },
+      child: Text(AppLocalizations.of(context).backHome),
+    ),
+  );
+}
+
+void alertSubmitErrors(BuildContext context, Future<UserAPIResult> future) {
+  future.then((result) {
+    switch (result) {
+      case UserAPIResult.serverError:
+        _validationDialog(
+            context, AppLocalizations.of(context).alertServerProblem);
+        break;
+      case UserAPIResult.clientError:
+        _validationDialog(
+            context, AppLocalizations.of(context).alertSubmitFailed);
+        break;
+      case UserAPIResult.accessTokenExpired:
+      case UserAPIResult.noRefreshToken:
+        _validationDialog(
+            context, AppLocalizations.of(context).alertSessionError);
+        break;
+      case UserAPIResult.noInternetConnection:
+        _validationDialog(
+            context, AppLocalizations.of(context).alertInternetConnection);
+        break;
+      case UserAPIResult.serverUnavailable:
+        _validationDialog(
+            context, AppLocalizations.of(context).alertServerMaintenance);
+        break;
+      default:
+        break;
+    }
+  });
+}
+
+Future<T> _validationDialog<T>(BuildContext context, String text) {
+  return showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      content: Text(text),
+    ),
+  );
+}
--- lib/screens/results/vocabulary.dart
+++ lib/screens/results/vocabulary.dart
@@ -1,5 +1,6 @@
 import 'package:dva232_project/client/user_api_client.dart';
 import 'package:dva232_project/routes.dart';
+import 'package:dva232_project/screens/results/shared.dart';
 import 'package:dva232_project/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:dva232_project/widgets/languide_navbar.dart';
@@ -27,33 +28,7 @@ class VocabularyResults extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    submitFuture.then((result) {
-      switch (result) {
-        case UserAPIResult.serverError:
-          _validationDialog(
-              context, AppLocalizations.of(context).alertServerProblem);
-          break;
-        case UserAPIResult.clientError:
-          _validationDialog(context,
-              "The app has failed to submit your result. Please report this issue to the developers.");
-          break;
-        case UserAPIResult.accessTokenExpired:
-        case UserAPIResult.noRefreshToken:
-          _validationDialog(context,
-              "Something went wrong with your session. Please login again.");
-          break;
-        case UserAPIResult.noInternetConnection:
-          _validationDialog(context,
-              AppLocalizations.of(context).alertInternetConnection);
-          break;
-        case UserAPIResult.serverUnavailable:
-          _validationDialog(context,
-              AppLocalizations.of(context).alertServerMaintenance);
-          break;
-        default:
-          break;
-      }
-    });
+    alertSubmitErrors(context, submitFuture);
 
     return WillPopScope(
       onWillPop: () async {
@@ -82,30 +57,9 @@ class VocabularyResults extends StatelessWidget {
       scrollDirection: Axis.vertical,
       padding: EdgeInsets.all(8.0),
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Your score is",
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              "${(100 * accuracy).toStringAsFixed(0)}%",
-              style: Theme.of(context).textTheme.headline1,
-            ),
-            Text("$score correct answers out of ${this.editedWords.length}"),
-          ],
-        ),
+        resultsDisplay(context, accuracy, score, this.editedWords.length),
         _answers(context),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.popUntil(context, ModalRoute.withName(Routes.home));
-            },
-            child: const Text("Back home"),
-          ),
-        ),
+        backHomeButton(context),
       ],
     );
   }
@@ -114,11 +68,11 @@ class VocabularyResults extends StatelessWidget {
     List<Widget> widgets = [
       ListTile(
         leading: Text(
-          "Correct answer",
+          AppLocalizations.of(context).correctAnswer,
           style: Theme.of(context).textTheme.bodyText1,
         ),
         trailing: Text(
-          "Your answer",
+          AppLocalizations.of(context).yourAnswer,
           style: Theme.of(context).textTheme.bodyText1,
         ),
       ),
@@ -142,15 +96,6 @@ class VocabularyResults extends StatelessWidget {
         style: correct
             ? LanGuideTheme.correctAnswerText(context)
             : LanGuideTheme.incorrectAnswerText(context),
-      ),
-    );
-  }
-
-  Future<T> _validationDialog<T>(BuildContext context, String text) {
-    return showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        content: Text(text),
       ),
     );
   }
